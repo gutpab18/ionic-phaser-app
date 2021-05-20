fa29b6928001b37708be4af84ac836831c657b55
AlexSt05 <alexstorandt@icloud.com>
2021-05-20 17:08:38 -0500

    platform

--- src/components/Platformer/Platformer.js
+++ src/components/Platformer/Platformer.js
@@ -13,18 +13,32 @@ export class Platformer extends Phaser.Scene
     {
         this.load.image('sky', 'assets/sky.png');
         this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.image('bomb', 'assets/bomb.png');
-        this.load.spritesheet('dude', 
-            'assets/dude.png',
-            { frameWidth: 32, frameHeight: 48 } 
-        ); 
+       // this.load.image('star', 'assets/star.png');
+        //this.load.image('bomb', 'assets/bomb.png');
+       //this.load.spritesheet('dude', 
+           // 'assets/dude.png',
+            //{ frameWidth: 32, frameHeight: 48 } 
+        //); 
     }
+    
 
-    create () 
-    {
+ create ()
+{
+    this.add.image(400, 300, 'sky');
+
+    this.platforms = this.physics.add.staticGroup();
+
+    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    this.platforms.create(600, 400, 'ground');
+    this.platforms.create(50, 250, 'ground');
+    this.platforms.create(750, 220, 'ground');
+}
+
+    
+    
         
-    }
+    
     update ()
     {
 
