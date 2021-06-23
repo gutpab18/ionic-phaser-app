c1bae1c9c69592a4bc9516e9000ae88c7b71d0b8
AlexSt05 <alexstorandt@icloud.com>
2021-06-23 17:58:30 -0500

    Phaser

--- src/components/Platformer/Platformer.js
+++ src/components/Platformer/Platformer.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
-
+var score = 0;
+var scoreText;
 
 
 export class Platformer extends Phaser.Scene
@@ -43,6 +44,8 @@ this.anims.create({
     frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
     frameRate: 10,
     repeat: -1
+
+
 });
 
 this.anims.create({
@@ -60,9 +63,65 @@ this.anims.create({
 this.player.body.setGravityY(300)
 this.physics.add.collider(this.player, this.platforms);
 this.cursors = this.input.keyboard.createCursorKeys();
-}
 
+this.stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
     
+});
+
+this.stars.children.iterate(function (child) {
+child.body.setGravityY(300)
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+});
+this.physics.add.collider(this.stars, this.platforms);
+
+this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+
+this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' }); 
+this.score = 0;
+
+this.bombs = this.physics.add.group();
+
+this.physics.add.collider(this.bombs, this.platforms);
+
+this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
+
+}
+hitBomb (player, bomb)
+{
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    this.gameOver = true;
+}
+collectStar (player,star)
+{
+    star.disableBody(true, true);
+    this.score += 10;
+    this.scoreText.setText('Score: ' + this.score);
+    if (this.stars.countActive(true) === 0)
+    {
+        this.stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = this.bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.setGravityY(300);
+    }
+}   
  
         
     
