9815b9829026e26983bc4e525c214f1ccd563862
AlexSt05 <alexstorandt@icloud.com>
2021-07-14 18:02:06 -0500

    game over

--- src/components/Platformer/PlatformerGameOver.js
+++ src/components/Platformer/PlatformerGameOver.js
@@ -20,9 +20,19 @@ export class PlatformerGameOver extends Phaser.Scene {
 
 
     create() {
-        this.add.image(400, 300, 'sky');     
-    }
+        this.add.image(400, 300, 'sky');  
+        var text = this.add.text(220, 100, '', { font: '64px Courier', fill: '#26c988' });
 
+        text.setText([
+            'You Lost...'
+        ]);   
+        
+    const helloButton = this.add.text(330,330 , 'Restart', { fill: '#26c988' });
+    helloButton.setInteractive();
+
+    helloButton.on('pointerover', () => {this.scene.start("Platformer")  });
+    }
+     
 
     update(time, delta) {
 
